b7113910c7bb4cf7c072125a4812de48f557a0e8
Masamune Takahashi <masamunedazeze@gmail.com>
2025-12-12 16:24:14 +0900

    tasksテーブルにstatusカラムを追加

--- database/migrations/2025_12_09_051009_create_tasks_table.php
+++ database/migrations/2025_12_09_051009_create_tasks_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->timestamps();
+            $table->boolean('status')->default(false);
         });
     }
 
