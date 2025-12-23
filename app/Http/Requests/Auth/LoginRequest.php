36cc1c0d9e2a890d8822451be4244c0355b52c18
Masamune Takahashi <masamunedazeze@gmail.com>
2025-12-23 16:45:08 +0900

    [#1] LoginRequestとRegisterRequestを作成

---
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+
+class LoginRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ];
+    }
+
+    /**
+     * Get custom messages for validator errors.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'email.required' => 'メールアドレスを入力してください。',
+            'email.email'    => 'メールアドレスの形式で入力してください。',
+
+            'password.required'  => 'パスワードを入力してください。',
+        ];
+    }
+
+    /**
+     * Attempt to authenticate the request's credentials.
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function authenticate(): void
+    {
+        if (! Auth::attempt($this->only('email','password'), $this->boolean('remember'))) {
+            throw ValidationException::withMessages([
+                'email' => 'ログインに失敗しました',
+            ]);
+        }
+    }
+}
+
